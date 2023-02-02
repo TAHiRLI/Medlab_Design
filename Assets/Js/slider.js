909ff66dc3cdad5daa36c1b97fcd1c564d639752
TAHiRLI <tahrli.tahirr@gmail.com>
2023-02-02 17:52:08 +0400

    index page about us section Done

--- Assets/Js/slider.js
+++ Assets/Js/slider.js
@@ -1,12 +1,14 @@
 
 $(document).ready(function () {
     $('.mainSlider').slick({
+        autoplay: true,
+        autoplaySpeed: 4000,
         dots: true,
-        dotClass:'slick-dots',
+        dotClass: 'slick-dots',
         infinite: true,
         speed: 500,
         fade: true,
         cssEase: 'linear',
-        arrows:false
+        arrows: false
     });
 });
