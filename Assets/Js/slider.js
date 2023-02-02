5663358730bf8f09e7452512b6c2b00268ae408c
TAHiRLI <tahrli.tahirr@gmail.com>
2023-02-02 12:59:24 +0400

    Slider sub responsive done

---
+++ Assets/Js/slider.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function () {
+    $('.mainSlider').slick({
+        dots: true,
+        dotClass:'slick-dots',
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        arrows:false
+    });
+});
