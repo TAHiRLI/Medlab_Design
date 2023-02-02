c628ef7b40b967072ee99b283a39e0f692ff0407
TAHiRLI <tahrli.tahirr@gmail.com>
2023-02-02 09:33:04 +0400

    navbar scroll done

--- Assets/Js/navbar.js
+++ Assets/Js/navbar.js
@@ -1,10 +1,37 @@
-let searchButton = document.getElementById("search-icon");
 
-console.log(searchButton);
 
-searchButton.addEventListener("click", (e)=>{
+
+
+
+
+// =============
+// Navbar Search
+// =============
+
+let searchButtons = document.getElementsByClassName("la-search");
+
+for (const searchButton of searchButtons) {
+
+   searchButton.addEventListener("click", (e)=>{
     var searchBox = e.target.parentElement;
     var input = e.target.previousElementSibling;
     if(!input.value?.length > 0)
     searchBox.classList.toggle("active")
+})
+
+}
+ 
+
+
+// =============
+// Navbar Scroll
+// =============
+window.addEventListener("scroll", ()=>{
+    console.log( document.documentElement.scrollTop)
+    if(document.body.scrollTop > 400 || document.documentElement.scrollTop >400){
+        document.getElementById("navbarScroll").style.top = "0";
+    }
+    else{
+        document.getElementById("navbarScroll").style.top = "-20%";
+    }
 })
