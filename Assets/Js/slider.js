4509cc56054ca8ce99892e819a075f31b1b0fae7
TAHiRLI <tahrli.tahirr@gmail.com>
2023-02-05 23:49:37 +0400

    aboutUs page layout done

--- Assets/Js/slider.js
+++ Assets/Js/slider.js
@@ -86,3 +86,44 @@ $('.valuesSlider').slick({
     autoplaySpeed: 2000,
     arrows:false
   });
+
+//=================================
+// Office slider
+//=================================
+
+$(document).ready(function () {
+    $('.galerySliderFor').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        draggable:true,
+        fade: true,
+        speed: 0,
+        arrows:true,
+        asNavFor: '.galerySliderNav',
+        prevArrow:"<button type='button' class='slick-prev slick-button pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+        nextArrow:"<button type='button' class='slick-next slick-button pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+    
+      });
+
+
+ 
+});
+
+
+   $('.galerySliderNav').slick({
+        infinite:true,
+        draggabe:true,
+        touchMove:true,
+        dotClass: 'slick-dots',
+        slidesToShow: 4,
+        slidesToScroll: 1,
+         asNavFor: '.galerySliderFor',
+        dots: false,
+        centerMode: false,
+        focusOnSelect: true,
+        arrows:true,
+        prevArrow:"<button type='button' class='slick-prev slick-button pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+        nextArrow:"<button type='button' class='slick-next slick-button pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+    
+    });
