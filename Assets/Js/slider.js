b6115498825506d3ff509c6e368e51e99b434d3d
TAHiRLI <tahrli.tahirr@gmail.com>
2023-02-14 13:17:49 +0400

    about us values responsive slider

--- Assets/Js/slider.js
+++ Assets/Js/slider.js
@@ -137,7 +137,24 @@ $('.valuesSlider').slick({
     autoplay: true,
     centerMode:false,
     autoplaySpeed: 2000,
-    arrows:false
+    arrows:false,
+    responsive: [
+
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+
+
+    ]
   });
 
 //=================================
